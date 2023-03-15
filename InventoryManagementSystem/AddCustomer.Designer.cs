995121f0ecb2d80e6d98d33f26afa28e514983f7
Chamith Dilshan <ssoftware190@gmail.com>
2023-03-15 09:52:55 +0530

    Create Customer Design

---
+++ InventoryManagementSystem/AddCustomer.Designer.cs
@@ -0,0 +1,317 @@
+﻿namespace InventoryManagementSystem
+{
+    partial class AddCustomer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            txtCustomerID = new Siticone.Desktop.UI.WinForms.SiticoneTextBox();
+            txtContactNumber = new Siticone.Desktop.UI.WinForms.SiticoneTextBox();
+            label3 = new Label();
+            txtCity = new Siticone.Desktop.UI.WinForms.SiticoneTextBox();
+            label4 = new Label();
+            txtAddress = new Siticone.Desktop.UI.WinForms.SiticoneTextBox();
+            label5 = new Label();
+            txtName = new Siticone.Desktop.UI.WinForms.SiticoneTextBox();
+            label6 = new Label();
+            btnAddCustomer = new Siticone.Desktop.UI.WinForms.SiticoneButton();
+            btnDiscard = new Siticone.Desktop.UI.WinForms.SiticoneButton();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.ForeColor = Color.FromArgb(56, 62, 73);
+            label1.Location = new Point(21, 25);
+            label1.Name = "label1";
+            label1.Size = new Size(136, 25);
+            label1.TabIndex = 22;
+            label1.Text = "New Customer";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 10.8F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.ForeColor = Color.FromArgb(72, 80, 94);
+            label2.Location = new Point(31, 117);
+            label2.Name = "label2";
+            label2.Size = new Size(91, 20);
+            label2.TabIndex = 23;
+            label2.Text = "Customer ID";
+            // 
+            // txtCustomerID
+            // 
+            txtCustomerID.BorderColor = Color.FromArgb(208, 213, 221);
+            txtCustomerID.BorderRadius = 8;
+            txtCustomerID.DefaultText = "";
+            txtCustomerID.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
+            txtCustomerID.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
+            txtCustomerID.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
+            txtCustomerID.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
+            txtCustomerID.FocusedState.BorderColor = Color.FromArgb(10, 73, 156);
+            txtCustomerID.Font = new Font("Segoe UI Emoji", 10.2F, FontStyle.Regular, GraphicsUnit.Point);
+            txtCustomerID.ForeColor = Color.FromArgb(72, 80, 94);
+            txtCustomerID.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
+            txtCustomerID.Location = new Point(172, 112);
+            txtCustomerID.Margin = new Padding(3, 4, 3, 4);
+            txtCustomerID.Name = "txtCustomerID";
+            txtCustomerID.PasswordChar = '\0';
+            txtCustomerID.PlaceholderText = "";
+            txtCustomerID.SelectedText = "";
+            txtCustomerID.Size = new Size(239, 33);
+            txtCustomerID.TabIndex = 25;
+            txtCustomerID.TextOffset = new Point(10, 0);
+            // 
+            // txtContactNumber
+            // 
+            txtContactNumber.BorderColor = Color.FromArgb(208, 213, 221);
+            txtContactNumber.BorderRadius = 8;
+            txtContactNumber.DefaultText = "";
+            txtContactNumber.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
+            txtContactNumber.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
+            txtContactNumber.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
+            txtContactNumber.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
+            txtContactNumber.FocusedState.BorderColor = Color.FromArgb(10, 73, 156);
+            txtContactNumber.Font = new Font("Segoe UI Emoji", 10.2F, FontStyle.Regular, GraphicsUnit.Point);
+            txtContactNumber.ForeColor = Color.FromArgb(72, 80, 94);
+            txtContactNumber.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
+            txtContactNumber.Location = new Point(172, 352);
+            txtContactNumber.Margin = new Padding(3, 4, 3, 4);
+            txtContactNumber.Name = "txtContactNumber";
+            txtContactNumber.PasswordChar = '\0';
+            txtContactNumber.PlaceholderText = "";
+            txtContactNumber.SelectedText = "";
+            txtContactNumber.Size = new Size(239, 33);
+            txtContactNumber.TabIndex = 27;
+            txtContactNumber.TextOffset = new Point(10, 0);
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 10.8F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.ForeColor = Color.FromArgb(72, 80, 94);
+            label3.Location = new Point(31, 357);
+            label3.Name = "label3";
+            label3.Size = new Size(118, 20);
+            label3.TabIndex = 26;
+            label3.Text = "Contact Number";
+            // 
+            // txtCity
+            // 
+            txtCity.BorderColor = Color.FromArgb(208, 213, 221);
+            txtCity.BorderRadius = 8;
+            txtCity.DefaultText = "";
+            txtCity.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
+            txtCity.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
+            txtCity.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
+            txtCity.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
+            txtCity.FocusedState.BorderColor = Color.FromArgb(10, 73, 156);
+            txtCity.Font = new Font("Segoe UI Emoji", 10.2F, FontStyle.Regular, GraphicsUnit.Point);
+            txtCity.ForeColor = Color.FromArgb(72, 80, 94);
+            txtCity.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
+            txtCity.Location = new Point(172, 285);
+            txtCity.Margin = new Padding(3, 4, 3, 4);
+            txtCity.Name = "txtCity";
+            txtCity.PasswordChar = '\0';
+            txtCity.PlaceholderText = "";
+            txtCity.SelectedText = "";
+            txtCity.Size = new Size(239, 33);
+            txtCity.TabIndex = 29;
+            txtCity.TextOffset = new Point(10, 0);
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 10.8F, FontStyle.Regular, GraphicsUnit.Point);
+            label4.ForeColor = Color.FromArgb(72, 80, 94);
+            label4.Location = new Point(31, 290);
+            label4.Name = "label4";
+            label4.Size = new Size(34, 20);
+            label4.TabIndex = 28;
+            label4.Text = "City";
+            label4.Click += label4_Click;
+            // 
+            // txtAddress
+            // 
+            txtAddress.BorderColor = Color.FromArgb(208, 213, 221);
+            txtAddress.BorderRadius = 8;
+            txtAddress.DefaultText = "";
+            txtAddress.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
+            txtAddress.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
+            txtAddress.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
+            txtAddress.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
+            txtAddress.FocusedState.BorderColor = Color.FromArgb(10, 73, 156);
+            txtAddress.Font = new Font("Segoe UI Emoji", 10.2F, FontStyle.Regular, GraphicsUnit.Point);
+            txtAddress.ForeColor = Color.FromArgb(72, 80, 94);
+            txtAddress.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
+            txtAddress.Location = new Point(172, 225);
+            txtAddress.Margin = new Padding(3, 4, 3, 4);
+            txtAddress.Name = "txtAddress";
+            txtAddress.PasswordChar = '\0';
+            txtAddress.PlaceholderText = "";
+            txtAddress.SelectedText = "";
+            txtAddress.Size = new Size(239, 33);
+            txtAddress.TabIndex = 31;
+            txtAddress.TextOffset = new Point(10, 0);
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 10.8F, FontStyle.Regular, GraphicsUnit.Point);
+            label5.ForeColor = Color.FromArgb(72, 80, 94);
+            label5.Location = new Point(31, 230);
+            label5.Name = "label5";
+            label5.Size = new Size(62, 20);
+            label5.TabIndex = 30;
+            label5.Text = "Address";
+            // 
+            // txtName
+            // 
+            txtName.BorderColor = Color.FromArgb(208, 213, 221);
+            txtName.BorderRadius = 8;
+            txtName.DefaultText = "";
+            txtName.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
+            txtName.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
+            txtName.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
+            txtName.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
+            txtName.FocusedState.BorderColor = Color.FromArgb(10, 73, 156);
+            txtName.Font = new Font("Segoe UI Emoji", 10.2F, FontStyle.Regular, GraphicsUnit.Point);
+            txtName.ForeColor = Color.FromArgb(72, 80, 94);
+            txtName.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
+            txtName.Location = new Point(172, 168);
+            txtName.Margin = new Padding(3, 4, 3, 4);
+            txtName.Name = "txtName";
+            txtName.PasswordChar = '\0';
+            txtName.PlaceholderText = "";
+            txtName.SelectedText = "";
+            txtName.Size = new Size(239, 33);
+            txtName.TabIndex = 33;
+            txtName.TextOffset = new Point(10, 0);
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 10.8F, FontStyle.Regular, GraphicsUnit.Point);
+            label6.ForeColor = Color.FromArgb(72, 80, 94);
+            label6.Location = new Point(31, 173);
+            label6.Name = "label6";
+            label6.Size = new Size(49, 20);
+            label6.TabIndex = 32;
+            label6.Text = "Name";
+            // 
+            // btnAddCustomer
+            // 
+            btnAddCustomer.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            btnAddCustomer.BorderColor = Color.FromArgb(10, 73, 156);
+            btnAddCustomer.BorderRadius = 4;
+            btnAddCustomer.BorderThickness = 2;
+            btnAddCustomer.Cursor = Cursors.Hand;
+            btnAddCustomer.DisabledState.BorderColor = Color.DarkGray;
+            btnAddCustomer.DisabledState.CustomBorderColor = Color.DarkGray;
+            btnAddCustomer.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            btnAddCustomer.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            btnAddCustomer.FillColor = Color.FromArgb(10, 73, 156);
+            btnAddCustomer.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            btnAddCustomer.ForeColor = Color.White;
+            btnAddCustomer.ImageOffset = new Point(0, 1);
+            btnAddCustomer.ImageSize = new Size(15, 10);
+            btnAddCustomer.Location = new Point(304, 507);
+            btnAddCustomer.Margin = new Padding(3, 2, 3, 2);
+            btnAddCustomer.Name = "btnAddCustomer";
+            btnAddCustomer.Size = new Size(107, 30);
+            btnAddCustomer.TabIndex = 43;
+            btnAddCustomer.Text = "Add Customer";
+            btnAddCustomer.TextOffset = new Point(2, 0);
+            // 
+            // btnDiscard
+            // 
+            btnDiscard.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            btnDiscard.BorderColor = Color.FromArgb(208, 211, 217);
+            btnDiscard.BorderRadius = 4;
+            btnDiscard.BorderThickness = 2;
+            btnDiscard.Cursor = Cursors.Hand;
+            btnDiscard.DisabledState.BorderColor = Color.DarkGray;
+            btnDiscard.DisabledState.CustomBorderColor = Color.DarkGray;
+            btnDiscard.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            btnDiscard.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            btnDiscard.FillColor = Color.White;
+            btnDiscard.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            btnDiscard.ForeColor = Color.FromArgb(93, 102, 121);
+            btnDiscard.ImageOffset = new Point(0, 1);
+            btnDiscard.ImageSize = new Size(15, 10);
+            btnDiscard.Location = new Point(199, 507);
+            btnDiscard.Margin = new Padding(3, 2, 3, 2);
+            btnDiscard.Name = "btnDiscard";
+            btnDiscard.Size = new Size(89, 30);
+            btnDiscard.TabIndex = 42;
+            btnDiscard.Text = "Discard";
+            btnDiscard.TextOffset = new Point(2, 0);
+            // 
+            // AddCustomer
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(441, 572);
+            ControlBox = false;
+            Controls.Add(btnAddCustomer);
+            Controls.Add(btnDiscard);
+            Controls.Add(txtName);
+            Controls.Add(label6);
+            Controls.Add(txtAddress);
+            Controls.Add(label5);
+            Controls.Add(txtCity);
+            Controls.Add(label4);
+            Controls.Add(txtContactNumber);
+            Controls.Add(label3);
+            Controls.Add(txtCustomerID);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "AddCustomer";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Siticone.Desktop.UI.WinForms.SiticoneTextBox txtCustomerID;
+        private Siticone.Desktop.UI.WinForms.SiticoneTextBox txtContactNumber;
+        private Label label3;
+        private Siticone.Desktop.UI.WinForms.SiticoneTextBox txtCity;
+        private Label label4;
+        private Siticone.Desktop.UI.WinForms.SiticoneTextBox txtAddress;
+        private Label label5;
+        private Siticone.Desktop.UI.WinForms.SiticoneTextBox txtName;
+        private Label label6;
+        private Siticone.Desktop.UI.WinForms.SiticoneButton btnAddCustomer;
+        private Siticone.Desktop.UI.WinForms.SiticoneButton btnDiscard;
+    }
+}
--- InventoryManagementSystem/UserControls/UC_Customers.Designer.cs
+++ InventoryManagementSystem/UserControls/UC_Customers.Designer.cs
@@ -28,33 +28,175 @@
         /// </summary>
         private void InitializeComponent()
         {
-            label1 = new Label();
+            DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle2 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle3 = new DataGridViewCellStyle();
+            siticonePanel1 = new Siticone.Desktop.UI.WinForms.SiticonePanel();
+            siticoneDataGridView1 = new Siticone.Desktop.UI.WinForms.SiticoneDataGridView();
+            customer_id = new DataGridViewTextBoxColumn();
+            name = new DataGridViewTextBoxColumn();
+            address = new DataGridViewTextBoxColumn();
+            city = new DataGridViewTextBoxColumn();
+            contact_number = new DataGridViewTextBoxColumn();
+            btnAddCustomer = new Siticone.Desktop.UI.WinForms.SiticoneButton();
+            lblCustomers = new Siticone.Desktop.UI.WinForms.SiticoneHtmlLabel();
+            siticonePanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)siticoneDataGridView1).BeginInit();
             SuspendLayout();
             // 
-            // label1
+            // siticonePanel1
             // 
-            label1.AutoSize = true;
-            label1.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
-            label1.Location = new Point(488, 383);
-            label1.Name = "label1";
-            label1.Size = new Size(160, 41);
-            label1.TabIndex = 3;
-            label1.Text = "Customers";
+            siticonePanel1.BackColor = Color.White;
+            siticonePanel1.Controls.Add(siticoneDataGridView1);
+            siticonePanel1.Controls.Add(btnAddCustomer);
+            siticonePanel1.Controls.Add(lblCustomers);
+            siticonePanel1.Dock = DockStyle.Fill;
+            siticonePanel1.Location = new Point(17, 14);
+            siticonePanel1.Name = "siticonePanel1";
+            siticonePanel1.Size = new Size(963, 580);
+            siticonePanel1.TabIndex = 0;
+            // 
+            // siticoneDataGridView1
+            // 
+            dataGridViewCellStyle1.BackColor = Color.White;
+            siticoneDataGridView1.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            siticoneDataGridView1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            dataGridViewCellStyle2.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = Color.White;
+            dataGridViewCellStyle2.Font = new Font("Segoe UI Semibold", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            dataGridViewCellStyle2.ForeColor = Color.FromArgb(93, 102, 121);
+            dataGridViewCellStyle2.SelectionBackColor = Color.White;
+            dataGridViewCellStyle2.SelectionForeColor = Color.FromArgb(10, 93, 156);
+            dataGridViewCellStyle2.WrapMode = DataGridViewTriState.True;
+            siticoneDataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            siticoneDataGridView1.ColumnHeadersHeight = 19;
+            siticoneDataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.EnableResizing;
+            siticoneDataGridView1.Columns.AddRange(new DataGridViewColumn[] { customer_id, name, address, city, contact_number });
+            dataGridViewCellStyle3.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = Color.White;
+            dataGridViewCellStyle3.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            dataGridViewCellStyle3.ForeColor = Color.FromArgb(71, 69, 94);
+            dataGridViewCellStyle3.SelectionBackColor = Color.FromArgb(231, 229, 255);
+            dataGridViewCellStyle3.SelectionForeColor = Color.FromArgb(71, 69, 94);
+            dataGridViewCellStyle3.WrapMode = DataGridViewTriState.False;
+            siticoneDataGridView1.DefaultCellStyle = dataGridViewCellStyle3;
+            siticoneDataGridView1.GridColor = Color.FromArgb(231, 229, 255);
+            siticoneDataGridView1.Location = new Point(24, 82);
+            siticoneDataGridView1.Name = "siticoneDataGridView1";
+            siticoneDataGridView1.RowHeadersVisible = false;
+            siticoneDataGridView1.RowTemplate.Height = 25;
+            siticoneDataGridView1.Size = new Size(918, 479);
+            siticoneDataGridView1.TabIndex = 3;
+            siticoneDataGridView1.ThemeStyle.AlternatingRowsStyle.BackColor = Color.White;
+            siticoneDataGridView1.ThemeStyle.AlternatingRowsStyle.Font = null;
+            siticoneDataGridView1.ThemeStyle.AlternatingRowsStyle.ForeColor = Color.Empty;
+            siticoneDataGridView1.ThemeStyle.AlternatingRowsStyle.SelectionBackColor = Color.Empty;
+            siticoneDataGridView1.ThemeStyle.AlternatingRowsStyle.SelectionForeColor = Color.Empty;
+            siticoneDataGridView1.ThemeStyle.BackColor = Color.White;
+            siticoneDataGridView1.ThemeStyle.GridColor = Color.FromArgb(231, 229, 255);
+            siticoneDataGridView1.ThemeStyle.HeaderStyle.BackColor = Color.FromArgb(100, 88, 255);
+            siticoneDataGridView1.ThemeStyle.HeaderStyle.BorderStyle = DataGridViewHeaderBorderStyle.None;
+            siticoneDataGridView1.ThemeStyle.HeaderStyle.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            siticoneDataGridView1.ThemeStyle.HeaderStyle.ForeColor = Color.White;
+            siticoneDataGridView1.ThemeStyle.HeaderStyle.HeaightSizeMode = DataGridViewColumnHeadersHeightSizeMode.EnableResizing;
+            siticoneDataGridView1.ThemeStyle.HeaderStyle.Height = 19;
+            siticoneDataGridView1.ThemeStyle.ReadOnly = false;
+            siticoneDataGridView1.ThemeStyle.RowsStyle.BackColor = Color.White;
+            siticoneDataGridView1.ThemeStyle.RowsStyle.BorderStyle = DataGridViewCellBorderStyle.SingleHorizontal;
+            siticoneDataGridView1.ThemeStyle.RowsStyle.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            siticoneDataGridView1.ThemeStyle.RowsStyle.ForeColor = Color.FromArgb(71, 69, 94);
+            siticoneDataGridView1.ThemeStyle.RowsStyle.Height = 25;
+            siticoneDataGridView1.ThemeStyle.RowsStyle.SelectionBackColor = Color.FromArgb(231, 229, 255);
+            siticoneDataGridView1.ThemeStyle.RowsStyle.SelectionForeColor = Color.FromArgb(71, 69, 94);
+            // 
+            // customer_id
+            // 
+            customer_id.HeaderText = "Customer ID";
+            customer_id.Name = "customer_id";
+            // 
+            // name
+            // 
+            name.HeaderText = "Name";
+            name.Name = "name";
+            // 
+            // address
+            // 
+            address.HeaderText = "Adress";
+            address.Name = "address";
+            // 
+            // city
+            // 
+            city.HeaderText = "City";
+            city.Name = "city";
+            // 
+            // contact_number
+            // 
+            contact_number.HeaderText = "Contact Number";
+            contact_number.Name = "contact_number";
+            // 
+            // btnAddCustomer
+            // 
+            btnAddCustomer.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            btnAddCustomer.BorderColor = Color.FromArgb(10, 73, 156);
+            btnAddCustomer.BorderRadius = 4;
+            btnAddCustomer.BorderThickness = 2;
+            btnAddCustomer.Cursor = Cursors.Hand;
+            btnAddCustomer.DisabledState.BorderColor = Color.DarkGray;
+            btnAddCustomer.DisabledState.CustomBorderColor = Color.DarkGray;
+            btnAddCustomer.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            btnAddCustomer.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            btnAddCustomer.FillColor = Color.FromArgb(10, 73, 156);
+            btnAddCustomer.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            btnAddCustomer.ForeColor = Color.White;
+            btnAddCustomer.HoverState.BorderColor = Color.FromArgb(10, 73, 156);
+            btnAddCustomer.HoverState.FillColor = Color.FromArgb(10, 73, 156);
+            btnAddCustomer.ImageOffset = new Point(0, 1);
+            btnAddCustomer.ImageSize = new Size(15, 10);
+            btnAddCustomer.Location = new Point(817, 29);
+            btnAddCustomer.Margin = new Padding(3, 2, 3, 2);
+            btnAddCustomer.Name = "btnAddCustomer";
+            btnAddCustomer.Size = new Size(110, 30);
+            btnAddCustomer.TabIndex = 2;
+            btnAddCustomer.Text = "Add Customer";
+            btnAddCustomer.TextOffset = new Point(2, 0);
+            btnAddCustomer.Click += btnAddCustomer_Click;
+            // 
+            // lblCustomers
+            // 
+            lblCustomers.BackColor = Color.Transparent;
+            lblCustomers.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            lblCustomers.Location = new Point(39, 36);
+            lblCustomers.Name = "lblCustomers";
+            lblCustomers.Size = new Size(81, 23);
+            lblCustomers.TabIndex = 0;
+            lblCustomers.Text = "Customers";
             // 
             // UC_Customers
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(240, 241, 243);
-            Controls.Add(label1);
+            Controls.Add(siticonePanel1);
+            Margin = new Padding(3, 2, 3, 2);
             Name = "UC_Customers";
-            Size = new Size(1136, 807);
+            Padding = new Padding(17, 14, 19, 16);
+            Size = new Size(999, 610);
+            siticonePanel1.ResumeLayout(false);
+            siticonePanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)siticoneDataGridView1).EndInit();
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
 
-        private Label label1;
+        private Siticone.Desktop.UI.WinForms.SiticonePanel siticonePanel1;
+        private Siticone.Desktop.UI.WinForms.SiticoneHtmlLabel lblCustomers;
+        private Siticone.Desktop.UI.WinForms.SiticoneButton btnAddCustomer;
+        private Siticone.Desktop.UI.WinForms.SiticoneDataGridView siticoneDataGridView1;
+        private DataGridViewTextBoxColumn customer_id;
+        private DataGridViewTextBoxColumn name;
+        private DataGridViewTextBoxColumn address;
+        private DataGridViewTextBoxColumn city;
+        private DataGridViewTextBoxColumn contact_number;
     }
 }
