b10dde592c844b39160039be102b216f7d97cea6
Chamith Dilshan <ssoftware190@gmail.com>
2023-03-15 11:57:41 +0530

    Added data insert,view.

--- InventoryManagementSystem/AddCustomer.cs
+++ InventoryManagementSystem/AddCustomer.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using InventoryManagementSystem.DataModels;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
 using System.Drawing;
 using System.Linq;
@@ -12,14 +14,39 @@ namespace InventoryManagementSystem
 {
     public partial class AddCustomer : Form
     {
+        private readonly MongoConnector _mongoConnector;
         public AddCustomer()
         {
             InitializeComponent();
+
+            string connectionString = ConfigurationManager.AppSettings["ConnectionString"]!;
+            _mongoConnector = new MongoConnector(connectionString, "InventoryManagementSystem");
         }
 
         private void label4_Click(object sender, EventArgs e)
         {
 
+        }
+
+        //Add Customer Button
+        private async void btnAddCustomer_Click(object sender, EventArgs e)
+        {
+            Customer customer = new Customer
+            {
+                customer_id = txtCustomerID.Text,
+                name = txtName.Text,
+                address = txtAddress.Text,
+                city =  txtCity.Text,
+                phone_no = txtContactNumber.Text
+            };
+
+            await _mongoConnector.Insert<Customer>("customers", customer);
+        }
+
+        //Discard button
+        private void btnDiscard_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
--- InventoryManagementSystem/UserControls/UC_Customers.cs
+++ InventoryManagementSystem/UserControls/UC_Customers.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
 using System.Drawing;
 using System.Linq;
@@ -12,15 +13,54 @@ namespace InventoryManagementSystem.UserControls
 {
     public partial class UC_Customers : UserControl
     {
+        private readonly MongoConnector _mongoConnector;
         public UC_Customers()
         {
             InitializeComponent();
+
+            string connectionString = ConfigurationManager.AppSettings["ConnectionString"]!;
+            _mongoConnector = new MongoConnector(connectionString, "InventoryManagementSystem");
         }
 
         private void btnAddCustomer_Click(object sender, EventArgs e)
         {
             AddCustomer addCustomer = new AddCustomer();
             addCustomer.ShowDialog();
+        }
+
+        private void UC_Customers_Load(object sender, EventArgs e)
+        {
+            CustomersLoad();
+        }
+
+        private async void CustomersLoad()
+        {
+            // Disable automatic row height adjustment
+            dgvCustomers.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.None;
+
+            //get all the items from the collection items
+            var documents = await _mongoConnector.GetAllCustomers();
+
+            // Process the result
+            foreach (var document in documents)
+            {
+
+                dgvCustomers.Rows.Add(new object[] {
+                    document.customer_id!,
+                    document.name!,
+                    document.address!,
+                    document.city!,
+                    document.phone_no!,
+                });
+            }
+
+            foreach (DataGridViewRow row in dgvCustomers.Rows)
+            {
+                row.Height = 50;
+            }
+
+            // set the current cell to null to prevent the first row from being selected
+            dgvCustomers.CurrentCell = null;
         }
     }
 }
