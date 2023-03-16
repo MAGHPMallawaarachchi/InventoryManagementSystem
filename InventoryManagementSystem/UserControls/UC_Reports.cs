3f8a6e738468732aeedf2d00cbc122352f7936ba
Hasini Punsala <hasinipunsala@gmail.com>
2023-03-16 09:57:25 +0530

    update UC_Reports.cs

--- InventoryManagementSystem/UserControls/UC_Reports.cs
+++ InventoryManagementSystem/UserControls/UC_Reports.cs
@@ -1,80 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
-namespace InventoryManagementSystem.UserControls
+﻿namespace InventoryManagementSystem.UserControls
 {
     public partial class UC_Reports : UserControl
     {
         public UC_Reports()
         {
             InitializeComponent();
-        }
-
-        private void siticoneButton1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void siticonePanel1_Paint(object sender, PaintEventArgs e)
-        {
-
-        }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void siticonePanel2_Paint(object sender, PaintEventArgs e)
-        {
-
-        }
-
-        private void siticonePanel3_Paint(object sender, PaintEventArgs e)
-        {
-
-        }
-
-        private void siticoneHtmlLabel1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void siticoneHtmlLabel2_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void dgvBestSellingCategories_CellContentClick(object sender, DataGridViewCellEventArgs e)
-        {
-
-        }
-
-        private void siticoneDataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
-        {
-
-        }
-
-        private void lblBestSellingCategories_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void dgvOverview_CellContentClick(object sender, DataGridViewCellEventArgs e)
-        {
-
-        }
-
-        private void siticonePanel1_Paint_1(object sender, PaintEventArgs e)
-        {
-
         }
     }
 }
